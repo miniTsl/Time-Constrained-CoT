8670f43db64b39c045d6cbacef8990467161f2ae
miniTsl <1668685245@qq.com>
2025-01-14 09:55:51 +0800

    统一运行脚本的格式

--- sh/mistral.sh
+++ sh/mistral.sh
@@ -2,10 +2,22 @@
 MODEL_LIST=(
     "mistralai/Mistral-7B-Instruct-v0.3"
     "mistralai/Ministral-8B-Instruct-2410"
-    # "mistralai/Mistral-Nemo-Instruct-2407"
-    # "mistralai/Mistral-Small-Instruct-2409"
+    "mistralai/Mistral-Nemo-Instruct-2407"
+    "mistralai/Mistral-Small-Instruct-2409"
     "mistralai/Mathstral-7B-v0.1"
 )
+
+
+PROMPT_TYPE_LIST=(
+    "sbs"
+    "c2f"
+    "aav"
+    "kf"
+    "sbs-hard"
+)
+
+PROMPT_PREFIX="mistral"
+OUTPUT_DIR=/data03/sunyi/time_constrained_cot/outputs/1_10
 
 # 遍历模型列表
 for MODEL_NAME_OR_PATH in "${MODEL_LIST[@]}"; do
@@ -13,49 +25,17 @@ for MODEL_NAME_OR_PATH in "${MODEL_LIST[@]}"; do
     echo "CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES}"
     echo "Processing model: ${MODEL_NAME_OR_PATH}"
     
-    # step-by-step hard
-    PROMPT_TYPE="mistral-sbs-hard"
-    BUDGET=1
-    OUTPUT_DIR=outputs/1_10
 
-    bash sh/eval_hard.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-
-    # step-by-step
-    PROMPT_TYPE="mistral-sbs"
-    BUDGET=-1
-    OUTPUT_DIR=outputs/1_10
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-    BUDGET=1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-
-    # coarse-to-fine
-    PROMPT_TYPE="mistral-c2f"
-    OUTPUT_DIR=outputs/1_10
-    BUDGET=-1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-    BUDGET=1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-
-    # knowledge-first
-    PROMPT_TYPE="mistral-kf"
-    OUTPUT_DIR=outputs/1_10
-    BUDGET=-1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-    BUDGET=1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-    # aav
-    PROMPT_TYPE="mistral-aav"
-    OUTPUT_DIR=outputs/1_10
-    BUDGET=-1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
-
-    BUDGET=1
-    bash sh/eval.sh ${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
+    for PROMPT_TYPE in "${PROMPT_TYPE_LIST[@]}"; do
+        # step-by-step hard
+        if [[ ${PROMPT_TYPE} == "sbs-hard" ]]; then
+            BUDGET=1
+            bash sh/eval.sh ${PROMPT_PREFIX}-${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
+        else
+            BUDGET=-1
+            bash sh/eval.sh ${PROMPT_PREFIX}-${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
+            BUDGET=1
+            bash sh/eval.sh ${PROMPT_PREFIX}-${PROMPT_TYPE} ${MODEL_NAME_OR_PATH} ${BUDGET} ${OUTPUT_DIR}
+        fi
+    done
 done
--- sh/qwen.sh
+++ sh/qwen.sh
@@ -22,7 +22,7 @@ OUTPUT_DIR=/data03/sunyi/time_constrained_cot/outputs/1_10
 
 # 遍历模型列表
 for MODEL_NAME_OR_PATH in "${MODEL_LIST[@]}"; do
-    export CUDA_VISIBLE_DEVICES="4,5,6,7"
+    export CUDA_VISIBLE_DEVICES="4,5,6,7"   # 1.5B needs 2 GPUs
     echo "CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES}"
     echo "Processing model: ${MODEL_NAME_OR_PATH}"
     
